a99f9e495e5320e8ab745e5b31990ef228e802d0
Rodigo G Martins <mrgobetti0@gmail.com>
2024-11-07 22:20:59 -0300

    Correção forms/livros

--- livros/forms.py
+++ livros/forms.py
@@ -20,12 +20,18 @@
 
 # -- Tentativa 3
 from django import forms
-from .models import Livro, Autor
+from .models import Livro
+from autores.models import Autor
+from django.forms.models import ModelChoiceField
 
 class LivroForm(forms.ModelForm):
+    autor = ModelChoiceField(queryset=Autor.objects.all())
     class Meta:
         model = Livro
         fields = ['nome', 'autor', 'categoria', 'quantidade_paginas', 'isbn']
-        widgets = {
-            'autor': forms.Select(attrs={'class': 'form-control'}),
-        }
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['autor'] = ModelChoiceField(queryset=Autor.objects.all())
+
+# autor = ModelChoiceField(queryset=Livro.objects.all())
